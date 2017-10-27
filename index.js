2a94e7b551b6038e89028670e77c098f846e1684
Dallin Parker <dallin.r.parker@gmail.com>
2017-10-26 20:50:29 -0400

    working

--- index.js
+++ index.js
@@ -1,28 +1,77 @@
 const shx = require('shelljs');
-const moment = require('moment');
+
+console.time('start');
+shx.cd('..')
+const dirNames = []
+let totalSizeRemoved = 0
+const startDir = shx.pwd().stdout
 
 
-shx.cd('..');
+shx.ls('-l').forEach(({name, atime}, i) => (
+    dirNames.push({ num: i, name, lastAccessed: atime, complete: null }))
+);
+
+let removeContainer = n => {
+    let {num, name, lastAccessed} = dirNames[n];
+    let cwd = shx.pwd().stdout;
+    reset(cwd);
+
+    switch (n <= 0) {
+        case true: return;
+            break;
+        case false: deleteDir(name)
+            break;
+        default: console.log('something has gone wrong');
+    }
+    removeContainer(n - 1)
+}
+removeContainer(dirNames.length - 1)
+
+
+function reset(cwd) {
+    (cwd !== startDir) ? shx.cd('..') : null;
+}
+
+function deleteDir(dir) {
+    shx.cd(dir)
+    shx.ls('-l').forEach(({name, size}) => {
+        if(name === 'node_modules') {
+            console.log('inside: ', shx.pwd().stdout);
+            totalSizeRemoved = totalSizeRemoved + size;
+            shx.rm('-rf', name)
+            shx.cd('..')
+        } else {
+           return shx.pwd().stdout
+        }
+    })
+}
+console.timeEnd('start')
+// shx.echo(totalDirectories)
+// console.log('names: ', dirNames);
 // const some = shx.exec('ls -lh', {silent: true}).split(' ');
 // console.log('some: ', some);
 
-shx.ls('-l').forEach(file => {
-    let { atime, ctime, mtime, birthtime, size, name} = file;
-    if(name === 'video') {
-        shx.cd('video');
-        shx.echo(process.cwd());
-        shx.ls('-l').forEach(v => console.log(v))
-    }
-    // let aTime = moment(atime).startOf('day').fromNow();
-    // let cTime = moment(ctime).startOf('day').fromNow();
-    // let mTime = moment(mtime).startOf('day').fromNow();
-    // let birth = moment(birthtime).startOf('day').fromNow();
-    // console.log('ACCESSED: ', aTime);
-    // console.log('CHANGED: ', cTime);
-    // console.log('MODIFIED: ', mTime);
-    // console.log('BIRTHED: ', birth);
-    // console.log(`the file ${name} is ${size}`);
-});
+// let dirRemover = n => {
+//     console.log('inside dir: ')
+// }
+
+// shx.ls('-l').forEach(dir => {
+//     let { atime, ctime, mtime, birthtime, size, name} = dir;
+//     console.log('name: ', name);
+//     shx.exec(`cd ${name}`);
+//         shx.ls('-l');
+//
+//     // let aTime = moment(atime).startOf('day').fromNow();
+//     // let cTime = moment(ctime).startOf('day').fromNow();
+//     // let mTime = moment(mtime).startOf('day').fromNow();
+//     // let birth = moment(birthtime).startOf('day').fromNow();
+//     // console.log('ACCESSED: ', aTime);
+//     // console.log('CHANGED: ', cTime);
+//     // console.log('MODIFIED: ', mTime);
+//     // console.log('BIRTHED: ', birth);
+//     // console.log(`the file ${name} is ${size}`);
+// });
+
 
 
 
